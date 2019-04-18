0dbd1b748acf4bdcb2b73b32e0ed82b975b4f38d
sirtezza451 <sirtezza451@outlook.com>
2019-04-18 22:43:09 +0930

    Fix spelling

--- launcher.py
+++ launcher.py
@@ -6,25 +6,22 @@ from subprocess import CalledProcessError
 
 from bot import Sparks
 
+
 def launch():
     try:
         with open("config.json") as f:
             config = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         with open("config.json", "w+") as f:
-            model = {
-                "token": "",
-                "deafult_prefix": "$",
-                "description": ""
-            }
-            
-            json.dump(model, f, indent = 4)
+            model = {"token": "", "default_prefix": "$", "description": ""}
+
+            json.dump(model, f, indent=4)
 
         with open("config.json") as f:
             config = json.load(f)
-        
+
         print("Please enter configuration parameters.")
-        
+
     if config["token"] is "":
         config["token"] = input("TOKEN: ")
 
@@ -33,8 +30,9 @@ def launch():
 
     with open("config.json", "w+") as f:
         json.dump(config, f, indent=4)
-           
+
     Sparks()
+
 
 if __name__ == "__main__":
     launch()
