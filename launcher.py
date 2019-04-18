4c9c41cd23490906a255c7b3dc74dc3a0188f1eb
Codian <44289933+xxcodianxx@users.noreply.github.com>
2019-04-18 23:11:40 +0930

    Better Terminal Formatting

--- launcher.py
+++ launcher.py
@@ -3,14 +3,12 @@
 import json
 import os
 import sys
-
-
 def launch():
     if sys.version_info[0] < 3:
         print("Python version 3 or higher required.")
         input("Press enter to exit.")
         raise SystemExit()
-
+      
     try:
         import discord
         from discord.ext import commands
@@ -19,7 +17,7 @@ def launch():
         print("Dependency error: {}".format(e))
         input("Press enter to exit.")
         raise SystemExit()
-
+ 
     try:
         with open("config.json") as f:
             config = json.load(f)
@@ -36,15 +34,12 @@ def launch():
 
     if config["token"] is "":
         config["token"] = input("TOKEN: ")
-
-    if config["description"] is "":
-        config["description"] = input("DESCRIPTION: ")
+        print("\n-----")
 
     with open("config.json", "w+") as f:
         json.dump(config, f, indent=4)
 
     from bot import Sparks
-
     Sparks()
 
 
