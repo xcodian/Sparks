c0e9ad9b9b6f6986d1a0f29be90c5c069e11b3e1
Codian <44289933+xxcodianxx@users.noreply.github.com>
2019-04-18 22:35:28 +0930

    fixed

--- launcher.py
+++ launcher.py
@@ -25,14 +25,15 @@ def launch():
         
         print("Please enter configuration parameters.")
         
-        if config["token"] is "":
-            config["token"] = input("TOKEN: ")
+    if config["token"] is "":
+        config["token"] = input("TOKEN: ")
 
-        if config["description"] is "":
-            config["description"] = input("DESCRIPTION: ")
+    if config["description"] is "":
+        config["description"] = input("DESCRIPTION: ")
 
-        with open("config.json", "w+") as f:
-            json.dump(config, f, indent=4)
+    with open("config.json", "w+") as f:
+        json.dump(config, f, indent=4)
+           
     Sparks()
 
 if __name__ == "__main__":
