5ee5bdacb7fcc1c96a824041a1eaabcae891a08f
sirtezza451 <sirtezza451@outlook.com>
2019-04-19 17:38:07 +0930

    Update status cog

--- cogs/status.py
+++ cogs/status.py
@@ -1,15 +1,21 @@
+# -*- coding: utf-8 -*-
+
 import discord
 from discord.ext import commands
 
 
-class botStatusCog(commands.Cog):
+class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="ping", brief="Get the connection latency.")
     async def ping(self, ctx):
-        embed = discord.Embed(title="", description=":stopwatch: {}ms".format(int(self.bot.latency * 1000)), color=discord.Color.blurple())
+        embed = discord.Embed(
+            description=":stopwatch: {}ms".format(int(self.bot.latency * 1000)),
+            color=discord.Color.blurple(),
+        )
         await ctx.send(embed=embed)
 
+
 def setup(bot):
-    bot.add_cog(botStatusCog(bot))
+    bot.add_cog(Status(bot))
