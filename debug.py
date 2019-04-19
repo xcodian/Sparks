93832833132337056e329376e424ee507abfa014
Codian <xxcodianxx@gmail.com>
2019-04-19 17:06:32 +0930

    Flag-parser bugfixes
    You can no longer have flags with no dash prefixing them.

--- debug.py
+++ debug.py
@@ -13,7 +13,7 @@ def end(text = "Press enter to exit."):
     return SystemExit
 
 # Provide any string such as "-flag1 flag1_arg1 -flag2 -flag2_arg1 flag2_arg2"
-# Provide how much args each flag takes as a dict {"flag1":1, "flag2":2}
+# Provide how much args each flag takes as a dict {"-flag1":1, "-flag2":2}
 
 # If there is a flag that demands an argument, it will take the next one in regardless of if it's got a "-" prefixing it.
 # eg. flags -flag2 takes 2 additional args so it will take both "-flag2_arg1" and "flag2_arg2" as its args.
@@ -24,13 +24,13 @@ def flagParse(txt, acc_flags):
     belongsto = None
 
     for i in txt.split(" "):
-        if (i.replace("-", "", 1) in acc_flags) and demand == 0:
-            demand = acc_flags[i.replace("-", "", 1)]  # get the demand of args for that flag
+        if (i in acc_flags) and demand == 0:
+            demand = acc_flags[i]  # get the demand of args for that flag
             output[i] = []
             belongsto = i  # mark any following ARGS as belonging to this flag
 
             # we don't wanna accept that one again
-            del acc_flags[i.replace("-", "", 1)]
+            del acc_flags[i]
 
         else:
             # is arg
