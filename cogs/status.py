1395322a6c1ff2f3a401a3b5e25b9b5d854d2252
Codian <xxcodianxx@gmail.com>
2019-04-19 18:44:35 +0930

    Finished uptime function.

--- cogs/status.py
+++ cogs/status.py
@@ -22,8 +22,13 @@ class Status(commands.Cog):
         embed = discord.Embed(colour=discord.Colour.blurple())
         embed.description = "A Discord bot created using [discord.py](https://github.com/Rapptz/discord.py)"
 
-        uptime = int(time.time() - self.bot.startTime)
-        embed.add_field(name="Uptime", value="{} seconds".format(uptime))
+        clock = [str(time.localtime()[2]), str(time.localtime()[3]), str(time.localtime()[4]), str(time.localtime()[5])]
+
+        uptime = []
+        for c, o in zip(clock, self.bot.startTime):
+            uptime.append(int(c) - int(o))
+
+        embed.add_field(name="Uptime", value="{0[0]} days, {0[1]} hours, {0[2]} minutes, {0[3]} seconds".format(uptime))
         embed.set_author(name="Sparks", url="https://github.com/xxcodianxx/Sparks")
         await ctx.send(embed=embed)
 
